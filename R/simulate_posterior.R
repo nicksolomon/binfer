119761dad68f8238c4464778b7d137be2ddb8d1c
Nick Solomon <nicksolomon10@gmail.com>
2018-01-06 01:07:07 -0500

    switch to mcmc::metrop

--- R/simulate_posterior.R
+++ R/simulate_posterior.R
@@ -1,17 +1,11 @@
-simulate_posterior <- function(x,
-                               ...) {
+simulate_posterior <- function(x, ...) {
   single_lik <- match.fun(attr(x, "likelihood"))
   prior <- match.fun(attr(x, "prior"))
   response <- attr(x, "response")
-  lik <- function(theta) {
-    x %>%
-      dplyr::mutate(.lik = single_lik(x[[response]], theta)) %>%
-      dplyr::pull(.lik) %>%
-      prod()
-  }
 
-  dens <- function(theta) {lik(theta) * prior(theta)}
+  dens <- function(theta) {log(prod(single_lik(x[[response]], theta) * prior(theta)))}
 
-  chain <- mh_sampler(dens, ...)
-  data.frame(chain = chain)
+  chain <- mcmc::metrop(dens, ...)
+
+  data.frame(chain = chain$batch)
 }
