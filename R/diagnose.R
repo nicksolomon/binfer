ac0120b7a25e6dd8ebb44b51334d01bf0a0ff021
Nick Solomon <nicksolomon10@gmail.com>
2018-01-07 19:18:02 -0500

    add some diagnostic plots

--- R/diagnose.R
+++ R/diagnose.R
@@ -1,13 +1,40 @@
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' @import patchwork
+#' @import ggplot2
+#' @import dplyr
+
 diagnose <- function(x) {
   with_diagnostics <- x %>%
     mutate(index = 1:nrow(x),
            accepted = chain == dplyr::lag(chain))
   message(paste0("Acceptance rate: ", mean(with_diagnostics$accepted, na.rm = TRUE)))
-  trace_plot <- ggplot2::ggplot(with_diagnostics, aes(x = index, y = chain)) +
+
+  trace_plot <- ggplot(with_diagnostics, aes(x = index, y = chain)) +
     geom_line()
-  dens_plot <- ggplot2::ggplot(with_diagnostics, aes(x = chain)) +
+
+  dens_plot <- ggplot(with_diagnostics, aes(x = chain)) +
     geom_density()
-  print(trace_plot)
-  print(dens_plot)
+
+  acf_df <- acf(with_diagnostics$chain, plot = FALSE)$acf %>%
+    data.frame(correlation = .) %>%
+    dplyr::mutate(lag = 0:(nrow(.) - 1))
+  conf_int_upper <- qnorm((1 + .95)/2) / sqrt(nrow(with_diagnostics))
+  conf_int_lower <- - conf_int_upper
+
+  acf_plot <- ggplot(acf_df, aes(x = lag, y = correlation)) +
+    geom_col() +
+    geom_hline(yintercept = conf_int_upper) +
+    geom_hline(yintercept = conf_int_lower)
+
+  plot_out <- (dens_plot + acf_plot) / trace_plot
+  print(plot_out)
   return(x)
 }
