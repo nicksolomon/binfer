05fffdc86c9aa58f0eb0d6534e8391b2214f2a9b
Nick Solomon <nicksolomon10@gmail.com>
2018-01-25 15:52:16 -0500

    Add taxis docs

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Taxi Data
+#'
+#' This data frame contains a random sample of yellow taxi rides given over the
+#' course of approximately an hour in New York City.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name taxis
+#' @usage data("taxis")
+#' @format A data frame
+NULL
