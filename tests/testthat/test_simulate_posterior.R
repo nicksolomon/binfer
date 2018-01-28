9ab48f323152783ff3c10957286940ecdc853c0e
Nick Solomon <nicksolomon10@gmail.com>
2018-01-27 20:37:05 -0500

    add tests for simulate_posterior

---
+++ tests/testthat/test_simulate_posterior.R
@@ -0,0 +1,30 @@
+library(binfer)
+library(dplyr)
+
+data("taxis")
+my_lik <- function(data, theta) {if (theta > 0) dpois(data, lambda = theta) else 0}
+my_prior <- function(theta) {dgamma(theta, rate = 10, shape = 10)}
+
+set.seed(20180127)
+
+taxis_small <- sample_frac(taxis, .01)
+
+to_simulate <- define(taxis_small, passenger_count ~ my_lik) %>%
+  assume(~ my_prior)
+
+set.seed(20180128)
+posterior <- simulate_posterior(to_simulate, initial = 1, nbatch = 100, scale = .01)
+
+context("Testing `simulate_posterior()`")
+
+test_that("Posterior mean is correct",{
+  expect_equal(mean(posterior$chain), 1.158674817, 1e-7)
+})
+
+test_that("Posterior sd is correct",{
+  expect_equal(sd(posterior$chain), 0.08477787, 1e-7)
+})
+
+test_that("Right number of simulations is output", {
+  expect_equal(nrow(posterior), 100)
+})
